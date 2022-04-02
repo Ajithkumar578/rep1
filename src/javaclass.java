abc7bbc56aa63f496c529ec5bc21a81901a69bf9
Ajithkumar578 <ajithkumarece15@gmail.com>
2022-04-02 03:11:58 -0700

    modified

--- src/javaclass.java
+++ src/javaclass.java
@@ -5,6 +5,10 @@ public class javaclass {
 		System.out.println("hi");
 		
 	}
+	public void b() {
+		System.out.println("hi");
+		
+	}
 	public static void main(String[] args) {
 		javaclass f=new javaclass();
 	}
